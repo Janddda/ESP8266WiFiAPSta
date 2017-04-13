b9bfbe5994148d5967e7d71611533c73077d8def
Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
2017-04-13 15:33:04 +0200

    Add header doc for example.

--- examples/ESP8266WiFiAPSta/ESP8266WiFiAPSta.ino
+++ examples/ESP8266WiFiAPSta/ESP8266WiFiAPSta.ino
@@ -1,5 +1,21 @@
+/**
+ *  @file     ESP8266WiFiAPSta.ino
+ *  @version  0.1
+ *  @date     April 2017
+ *  @author   Nhu-Hoai Robert VO <nhuhoai.vo@franicflow.ch>
+ *  @brief    Demo with the AP+Sta mode on ESP8266 & Web server config
+ */
+
+/**
+ *
+ */
 void setup() {
+  
 }
 
+/**
+ *  
+ */
 void loop() {
+  
 }
